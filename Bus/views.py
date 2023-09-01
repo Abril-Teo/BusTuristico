82d4b4cc029c6e6560fd031c1b8ce5584feef059
nachofillol05 <nachofillol05@gmail.com>
2023-09-01 12:05:57 -0300

    aniadi el index

--- Bus/views.py
+++ Bus/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Bus
+
 # Create your views here.
+
+def index(request):
+    buses = Bus.objects.all()
+    return render(request, 'index.html',{'bus': buses})
