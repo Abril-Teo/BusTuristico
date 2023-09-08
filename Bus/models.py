4ef8b6961e0ade994cd7226b97586f684cce729e
nachofillol05 <nachofillol05@gmail.com>
2023-09-08 11:16:35 -0300

    Cambios en el models

--- Bus/models.py
+++ Bus/models.py
@@ -92,7 +92,7 @@ class CambioEstado(models.Model):
     motivo = models.CharField(max_length=100)
     
     def __str__(self) -> str:
-        return self.fechaCambio
+        return "viejo: " + self.estadoNuevo.nombre + ", nuevo: " + self.estadoAnterior.nombre
 
 class Atractivo(models.Model):
     nombre = models.CharField(max_length= 45)
