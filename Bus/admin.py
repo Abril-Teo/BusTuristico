e18e0b96614c0061a4325680ef16325a1f68362d
nachofillol05 <nachofillol05@gmail.com>
2023-10-20 18:01:45 -0300

    cambios generales

--- Bus/admin.py
+++ Bus/admin.py
@@ -1,24 +1,25 @@
 from django.contrib import admin
 from .models import Atractivo, Bus, Chofer, Estado,CambioEstado, Parada, ParadaxRecorrido, Recorrido, Viaje
 from django.contrib.auth.models import User
-from django.contrib.auth.models import Permission
 
 class BusAdmin(admin.ModelAdmin):
     search_fields = ('patente','numUnidad')
     list_display = ('patente','numUnidad','fechaCompra')
     list_filter = ('patente',)
-class PermissionAdmin(admin.ModelAdmin):
-    list_display = ('codename','name')
     
     
 
 class ChoferAdmin(admin.ModelAdmin):
-    list_display = ['dni', 'legajo']
+    list_display = ['dni', 'legajo','nombre','apellido']
     def save_model(self, request, obj, form, change):
         username = obj.dni
         password = obj.legajo
+        name = obj.nombre
+        lastname = obj.apellido
         user, created = User.objects.get_or_create(username=username)
         user.set_password(password)
+        user.first_name = name
+        user.last_name = lastname
         user.is_staff = True
         user.save()
         obj.save()
@@ -33,4 +34,3 @@ admin.site.register(Parada)
 admin.site.register(Atractivo)
 admin.site.register(CambioEstado)
 admin.site.register(Recorrido)
-admin.site.register(Permission,PermissionAdmin)
